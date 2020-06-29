9fa6c323c0e5948e522c6bed303fa3a5f9a2a9de
syn <849899816@qq.com>
2020-06-29 21:51:06 +0800

    feat: 平衡二叉树

--- 110.平衡二叉树.js
+++ 110.平衡二叉树.js
@@ -16,8 +16,25 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function(root) {
+var isBalanced = function (root) {
+  //标志位
+  let flag = true
+  //递归处理
+  function dfs(root) {
+    if (!root || !flag) {
+      return 0
+    }
+    const left = dfs(root.left)
+    const right = dfs(root.right)
+    if (Math.abs(left - right) > 1) {
+      flag = false
+      return 0
+    }
+    return Math.max(left, right) + 1
 
+  }
+  dfs(root)
+  return flag
 };
 // @lc code=end
 
