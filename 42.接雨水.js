a3dea809cbdf4f7eddcd9ce67199ed6c8d5cd0dd
syn <849899816@qq.com>
2020-08-04 20:13:31 +0800

    feat: 对撞指针解决接雨水

--- 42.接雨水.js
+++ 42.接雨水.js
@@ -9,8 +9,27 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
+var trap = function (height) {
+  let leftCur = 0
+  let rightCur = height.length - 1
+  let res = 0
+  let leftMax = 0
+  let rightMax = 0
 
+  while (leftCur < rightCur) {
+    const left = height[leftCur]
+    const right = height[rightCur]
+    if (left < right) {
+      leftMax = Math.max(left, leftMax)
+      res += leftMax - left
+      leftCur++
+    } else {
+      rightMax = Math.max(right, rightMax)
+      res += rightMax - right
+      rightCur--
+    }
+  }
+  return res
 };
 // @lc code=end
 
