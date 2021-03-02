6452d9e1d7e0a3e0725c8390c61f6d0a12ce4f86
songyanan <songyanan@huohua.cn>
2021-03-02 10:48:04 +0800

    feat: 回溯+剪枝 处理青蛙过河

--- 403.青蛙过河.js
+++ 403.青蛙过河.js
@@ -9,8 +9,30 @@
  * @param {number[]} stones
  * @return {boolean}
  */
-var canCross = function(stones) {
+var canCross = function (stones) {
 
+  const helper = function (stones, index, k, set) {
+    const key = index * 1000 + k
+    if (set.has(key)) {
+      return false
+    } else {
+      set.add(key)
+    }
+    for (let i = index + 1; i < stones.length; i++) {
+      const gap = stones[i] - stones[index]
+      if (gap >= k - 1 && gap <= k + 1) {
+        if (helper(stones, i, gap, set)) {
+          return true
+        }
+      } else if (gap > k + 1) {
+        break
+      }
+    }
+    return index == stones.length - 1
+  }
+
+  const set = new Set()
+  return helper(stones, 0, 0, set)
 };
 // @lc code=end
 
