307e0ec9b20b2cdcf0f80273acb07c35de7476bc
songyanan <songyanan@huohua.cn>
2021-03-03 10:08:22 +0800

    feat: 数组·按部就班解决《一手顺子》

--- 846.一手顺子.js
+++ 846.一手顺子.js
@@ -10,8 +10,17 @@
  * @param {number} W
  * @return {boolean}
  */
-var isNStraightHand = function(hand, W) {
-
+var isNStraightHand = function (hand, W, min) {
+  while (hand.length && (min = Math.min(...hand)) > -1) {
+    for (let i = 0, j; i < W; i++) {
+      if ((j = hand.indexOf(min + i)) > -1) {
+        hand.splice(j, 1)
+      } else {
+        return false
+      }
+    }
+  }
+  return true
 };
 // @lc code=end
 
