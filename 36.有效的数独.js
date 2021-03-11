afe9132f01efc70c9065ae66723d471920f43c1f
songyanan <songyanan@huohua.cn>
2021-03-11 15:26:33 +0800

    feat: 双循环遍历解决 有效的数独

--- 36.有效的数独.js
+++ 36.有效的数独.js
@@ -20,7 +20,6 @@ var isValidSudoku = function (board) {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       if (board[i][j] === '.') continue
-      let num = board[i][j]
       //看看是哪个盒子
       let box_index = Math.floor(i / 3) * 3 + Math.floor(j / 3)
       if (box[box_index].has(board[i][j])) return false
