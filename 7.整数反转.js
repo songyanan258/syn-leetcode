f9f1cfdcbe06abdee634a0b35eb4bd0a5c1747dd
songyanan <songyanan@huohua.cn>
2021-04-14 19:38:25 +0800

    feat: 按部就班处理 整数反转

--- 7.整数反转.js
+++ 7.整数反转.js
@@ -11,14 +11,18 @@
  */
 var reverse = function (x) {
   let res = 0, mark = 1
+  //先保存下来符号
   if (x < 0) {
     mark = -1
   }
   x = Math.abs(x)
+  //每次取末尾一位进行反转
   while (x >= 1) {
     res = res * 10 + x % 10
+    //整数处理
     x = Math.floor(x / 10)
   }
+  //判断是否超出界限
   return res > Math.pow(2, 31) ? 0 : res * mark
 
 };
