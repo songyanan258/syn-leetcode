0036f0028d1b2b3c2535febef9fbfa0c2ab9d71c
songyanan <songyanan@huohua.cn>
2021-04-14 10:26:23 +0800

    feat: 异或运算解决 只出现一次的数字

--- 136.只出现一次的数字.js
+++ 136.只出现一次的数字.js
@@ -10,16 +10,21 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
-  nums.sort()
-  let left = 0, right = 1
-  while (left < nums.length) {
-    if (nums[left] === nums[right]) {
-      right += 2
-      left += 2
-    } else {
-      return nums[left]
-    }
-  }
+  //排序+双指针
+  // nums.sort()
+  // let left = 0, right = 1
+  // while (left < nums.length) {
+  //   if (nums[left] === nums[right]) {
+  //     right += 2
+  //     left += 2
+  //   } else {
+  //     return nums[left]
+  //   }
+  // }
+
+  //异或运算
+
+  return nums.reduce((a, b) => a ^ b)
 
 };
 // @lc code=end
