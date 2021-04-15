1e5459132062632eb06044c2edcdeb04bc4b5622
songyanan <songyanan@huohua.cn>
2021-04-15 10:11:08 +0800

    feat: 生成数组+指针极倔 验证回文串

--- 125.验证回文串.js
+++ 125.验证回文串.js
@@ -10,9 +10,20 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  if (s.length === 0) return true
-  const res = s.replace(/[^a-zA-Z\d]/g, '').toLowerCase()
-  return res.split('').reverse().join('') === res
+  //生成数组反转验证
+  // if (s.length === 0) return true
+  // const res = s.replace(/[^a-zA-Z\d]/g, '').toLowerCase()
+  // return res.split('').reverse().join('') === res
+  //生成数组双指针验证
+  let arr = s.match(/[0-9a-zA-Z]/g)
+  let index = 0
+  if (!arr) return true
+  while (index < arr.length / 2) {
+    if ((arr[index].toLowerCase() !== arr[arr.length - 1 - index].toLowerCase())) return false
+    index++
+  }
+
+  return true
 };
 // @lc code=end
 
