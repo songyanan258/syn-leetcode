80b61a4fbcf18a23d39871dd60d4b14b54e8319c
songyanan <songyanan@huohua.cn>
2021-07-09 16:33:56 +0800

    feat: 环形结构 解决旋转链表

--- 202.快乐数.js
+++ 202.快乐数.js
@@ -10,12 +10,14 @@
  * @return {boolean}
  */
 var isHappy = function (n, obj = {}) {
+  //判断链表是否有环
   if (obj.hasOwnProperty(typeof n + n)) return false
   obj[typeof n + n] = true
   const res = n.toString().split('').reduce((a, b) => {
     return a + Number(b) * Number(b)
   }, 0)
   return res === 1 ? true : isHappy(res, obj)
+
 };
 // @lc code=end
 
