daf8f6a40ea7631b37a6fb73061f2122bbb68ef2
songyanan <songyanan@huohua.cn>
2021-09-13 19:37:42 +0800

    feat: 用栈来解决 二叉树的中序遍历

--- 94.二叉树的中序遍历.js
+++ 94.二叉树的中序遍历.js
@@ -17,11 +17,23 @@
  * @return {number[]}
  */
 var inorderTraversal = function (root) {
-  let stack = [], res = []
-  root & stack.push(root)
-  while (stack.length > 0) {
-
+  //用栈来解决
+  let stack = []
+  let current = root
+  let ans = []
+  while (current || stack.length) {
+    if (current) {//先深度遍历，将左节点入栈
+      stack.push(current)
+      current = current.left
+    } else {
+      //向上依次遍历，记录结果
+      current = stack.pop()
+      ans.push(current.val)
+      //线上查找右边节点
+      current = current.right
+    }
   }
+  return ans
 };
 // @lc code=end
 
