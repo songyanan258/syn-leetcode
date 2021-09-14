07c6c791fd91c2232d026dd43330dc573a97c890
songyanan <songyanan@huohua.cn>
2021-09-14 16:44:01 +0800

    feat: 双栈 解决基本计算器

--- test.js
+++ test.js
@@ -1,23 +1,13 @@
-var setZeroes = function (matrix) {
-  let copyArr = new Array(matrix.length)
-  for (let i = 0; i < copyArr.length; i++) {
-    copyArr[i] = Object.assign([], matrix[i])
-  }
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      if (matrix[i][j] === 0) {
-        let row = 0, column = 0
-        while (row < matrix[0].length) {
-          copyArr[i][row] = 0
-          row++
-        }
-        while (column < matrix.length) {
-          copyArr[column][j] = 0
-          column++
-        }
-      }
-    }
-  }
-  return copyArr
-};
-let s = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+var preorderTraversal = function (root) {
+  let ans = []
+  preorder(root, ans)
+  return ans
+}
+
+const preorder = function (root, ans) {
+  if (!root) return
+  ans.push(root.val)
+  preorder(root.left, ans)
+  preorder(root.right, ans)
+  return
+}
